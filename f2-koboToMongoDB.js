76ab40dc9cb608dddb682759746903c2c86a3658
Taylor Downs <downs.taylor@gmail.com>
2020-05-05 17:19:18 -0600

    closes #2

--- f2-koboToMongoDB.js
+++ f2-koboToMongoDB.js
@@ -3,8 +3,23 @@ insertDocuments({
   database: 'medair',
   collection: 'beneficiaries',
   documents: (state) => {
-    return state.data.map((person) => {
-      return { name: person.name, age: person.age };
-    });
+    const person = state.data.data;
+    return [
+      {
+        // Bio Section
+        name: person.Beneficiary_First_Name + person.Beneficiary_Surname,
+        age: person.Beneficiary_Age,
+        program: person.Program,
+        registrationDate: person.Registration_Date,
+        gpsLocation: person._geolocation,
+
+        // Metadata Section
+        id: person._id,
+        uuid: person._uuid,
+        status: person._status,
+        tags: ['openfn', 'kobo'],
+        timestamp: person._submission_time,
+      },
+    ];
   },
 });
