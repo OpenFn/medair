9f97ab1efb56877e7d5c3cad6098fc615bced6f8
Taylor Downs <downs.taylor@gmail.com>
2020-05-05 15:27:13 -0600

    load documents to mongo

--- f2-koboToMongoDB.js
+++ f2-koboToMongoDB.js
@@ -1,1 +1,10 @@
-//Flow 2: Job to upload Kobo form to central MongoDB
+// Flow 2: Job to upload Kobo form to central MongoDB
+insertDocuments({
+  database: 'medair',
+  collection: 'beneficiaries',
+  documents: (state) => {
+    return state.data.map((ben) => {
+      return { name: ben.name, age: ben.age };
+    });
+  },
+});
