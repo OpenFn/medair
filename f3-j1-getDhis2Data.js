3e5652a48cc90367ba0f68f4cbbfae3f1f710768
Aleksa Krolls <aleksagk@gmail.com>
2020-05-06 07:11:22 -0700

    aleksa@openfn.org's change on OpenFn.org

--- f3-j1-getDhis2Data.js
+++ f3-j1-getDhis2Data.js
@@ -1,12 +1,8 @@
 //Flow 3, Job 1 to fetch data from DHIS2 play instance
 fetchEvents({
   fields: {
-    //orgUnit: 'BfMAe6Itzgt',
     orgUnit: 'g8upMTyEZGZ',
     profram: 'IpHINAT79UW',
-    lastUpdated: '2019-08-21',
-    //lastUpdated: '2019-08-21T13:29:37.255',
-    //program: 'lxAQ7Zs9VYR',
-    //endDate: '2021-01-01',
+    lastUpdated: '2019-08-21', //this will return a smaller payload; speak to how filtering could be dynamic
   },
 });
