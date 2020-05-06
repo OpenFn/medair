f084d259e669d5b822e799d2918298ac5d20cd6f
Taylor Downs <taylor@openfn.org>
2020-05-06 07:37:32 -0600

    taylor@openfn.org's change on OpenFn.org

--- f3-j1-getDhis2Data.js
+++ f3-j1-getDhis2Data.js
@@ -1,8 +1,8 @@
 //Flow 3, Job 1 to fetch data from DHIS2 play instance
 fetchEvents({
   fields: {
-    orgUnit: 'DiszpKrYNg8',
-    program: 'eBAyeGv0exc',
-    endDate: '2016-01-01',
+    orgUnit: 'BfMAe6Itzgt',
+    program: 'Rp268JB6Ne4',
+    endDate: '2021-01-01',
   },
 });
