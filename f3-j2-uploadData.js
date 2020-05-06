ff4b6b8e03c610973d0c6e7131eb6efca50238fe
Taylor Downs <downs.taylor@gmail.com>
2020-05-05 18:44:36 -0600

    template for #3

--- f3-j2-uploadData.js
+++ f3-j2-uploadData.js
@@ -1,1 +1,19 @@
 //Flow 3, Job 2 to load DHIS2 data to MungoDB
+insertDocuments({
+  database: 'medair',
+  collection: 'events',
+  documents: (state) => {
+    console.log('DHIS2 says: ' + JSON.stringify(state.data));
+    const event = state.data.data;
+    return [
+      {
+        // Metadata Section
+        id: event._id,
+        uuid: event._uuid,
+        event_status: event._status,
+        tags: ['openfn', 'dhis2'],
+        timestamp: event._submission_time,
+      },
+    ];
+  },
+});
