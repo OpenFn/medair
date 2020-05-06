d1ad47dfd48f049104dfe3db72df1a5a2b540a9f
Taylor Downs <downs.taylor@gmail.com>
2020-05-05 18:43:00 -0600

    template for #3

--- f3-j1-getDhis2Data.js
+++ f3-j1-getDhis2Data.js
@@ -1,1 +1,20 @@
 //Flow 3, Job 1 to fetch data from DHIS2 play instance
+get(
+  '/demo/api/33/dataValueSets',
+  {
+    query: {
+      dataSet: 'pBOMPrpg1QX',
+      dataSet: 'BfMAe6Itzgt',
+      startDate: '2013-01-01',
+      endDate: '2020-01-31',
+      orgUnit: 'YuQRtpLP10I',
+      orgUnit: 'vWbkYPRmKyS',
+      children: 'true',
+    },
+    headers: { 'content-type': 'application/json' },
+  },
+  state => {
+    delete state.references;
+    return state;
+  }
+);
