c2fe328e63881ab05c2c859085f29e1ffa91ec4e
Taylor Downs <downs.taylor@gmail.com>
2020-05-07 06:53:43 -0600

    closes #5 by adding a last 24 hours filter

--- f3-j1-getDhis2Data.js
+++ f3-j1-getDhis2Data.js
@@ -1,15 +1,16 @@
 //Flow 3, Job 1 to fetch data from DHIS2 play instance
 
-alterState(state => {
+alterState((state) => {
   state.references = [];
   return state;
-})
+});
 
 fetchEvents({
   fields: {
     orgUnit: 'g8upMTyEZGZ',
-    lastUpdated: '2019-08-21', //this will return a smaller payload; speak to how filtering could be dynamic
-    //lastUpdated: 'last24HoursFromNow` 
+    lastUpdated: new Date(Date.now() - 24 * 60 * 60 * 1000)
+      .toISOString()
+      .slice(0, 10),
     //program: 'M3xtLkYBlKI', //we can choose to add other filter paramters supported by DHIS2 api
   },
 });
