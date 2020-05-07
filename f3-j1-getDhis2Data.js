0703d2f2a3922470c2c4917a649f9be49ac6541a
Aleksa Krolls <aleksagk@gmail.com>
2020-05-06 17:35:36 -0700

    Update f3-j1-getDhis2Data.js

--- f3-j1-getDhis2Data.js
+++ f3-j1-getDhis2Data.js
@@ -8,6 +8,8 @@ alterState(state => {
 fetchEvents({
   fields: {
     orgUnit: 'g8upMTyEZGZ',
-    //lastUpdated: '2019-08-21', //this will return a smaller payload; speak to how filtering could be dynamic
+    lastUpdated: '2019-08-21', //this will return a smaller payload; speak to how filtering could be dynamic
+    //lastUpdated: 'last24HoursFromNow` 
+    //program: 'M3xtLkYBlKI', //we can choose to add other filter paramters supported by DHIS2 api
   },
 });
