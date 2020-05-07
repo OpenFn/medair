db4564ce78a9186691323627512bc3f472f2b126
Aleksa Krolls <aleksagk@gmail.com>
2020-05-07 06:36:36 -0700

    aleksa@openfn.org's change on OpenFn.org

--- f1-koboToDHIS2.js
+++ f1-koboToDHIS2.js
@@ -1,28 +1,27 @@
 //Flow 1: Job to upload Kobo form to DHIS2 play instance
 createTEI({ //Helper function to create Tracked Entity Instance
-  trackedEntityType: 'nEenWmSyUEp',
-  orgUnit: 'g8upMTyEZGZ',
-  attributes: [
-    {
-      attribute: 'w75KJ2mc4zz', // Attribute Id for FirstName in DHIS2
-      value: dataValue('data.Beneficiary_First_Name')(state), //Question in Kobo form
-    },
-    {
-      attribute: 'zDhUuAYrxNC', 
-      value: dataValue('data.Beneficiary_Surname')(state),
-    },
-    /*{
+    "trackedEntityType": "nEenWmSyUEp",
+    "orgUnit": "g8upMTyEZGZ",
+    "attributes": [
+      {
+        "attribute": "w75KJ2mc4zz", // Attribute Id for FirstName
+        "value": dataValue("data.Beneficiary_First_Name")(state) //Question in Kobo form
+      },
+      {
+        "attribute": "zDhUuAYrxNC", 
+        "value": dataValue("data.Beneficiary_Surname")(state)
+      }
+      /*{
       attribute: 'attributeId', // Other DHIS2 attributes...
       value: sourceValue, //Map to Kobo source...
     },*/
-  ],
-  enrollments: [
-    {
-      orgUnit: 'g8upMTyEZGZ',
-      program: 'fDd25txQckK',
-     // program: 'IpHINAT79UW', //specific program id
-      enrollmentDate: dataValue('data._submission_time')(state).substring(0, 10), //remove timeStamp from date
-      incidentDate: dataValue('data._submission_time')(state).substring(0, 10),
-    },
-  ],
-});
+    ],
+    "enrollments": [
+      {
+          "orgUnit": "g8upMTyEZGZ",
+          "program": "IpHINAT79UW", 
+          "enrollmentDate": dataValue("data._submission_time")(state).substring(0,10), //only return YYYY-MM-DD, no timestamp
+          "incidentDate": dataValue("data._submission_time")(state).substring(0,10)
+       }
+     ]
+  });
