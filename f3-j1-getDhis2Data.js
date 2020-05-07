4c69c784bd47feef4e4f8125825ce18c9f54a9f9
Aleksa Krolls <aleksagk@gmail.com>
2020-05-06 17:24:40 -0700

    Remove program

--- f3-j1-getDhis2Data.js
+++ f3-j1-getDhis2Data.js
@@ -8,7 +8,6 @@ alterState(state => {
 fetchEvents({
   fields: {
     orgUnit: 'g8upMTyEZGZ',
-    profram: 'IpHINAT79UW',
     lastUpdated: '2019-08-21', //this will return a smaller payload; speak to how filtering could be dynamic
   },
 });
