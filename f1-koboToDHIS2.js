0ef75dcd047a96647a8954cf1ce149ce561a8bf4
Aleksa Krolls <aleksagk@gmail.com>
2020-05-06 17:13:07 -0700

    aleksa@openfn.org's change on OpenFn.org

--- f1-koboToDHIS2.js
+++ f1-koboToDHIS2.js
@@ -6,7 +6,6 @@ createTEI({
     {
       attribute: 'w75KJ2mc4zz', // Attribute Id for FirstName in DHIS2
       value: dataValue('data.Beneficiary_First_Name'), //Question in Kobo form
-      //state.data.data.Beneficiary_First_Name
     },
     {
       attribute: 'zDhUuAYrxNC', // LastName attribute
@@ -21,7 +20,7 @@ createTEI({
     {
       orgUnit: 'g8upMTyEZGZ',
       program: 'IpHINAT79UW', //specific program id
-      enrollmentDate: dataValue('data._submission_time')(state).substring(0, 10),
+      enrollmentDate: dataValue('data._submission_time')(state).substring(0, 10), //remove timeStamp from date
       incidentDate: dataValue('data._submission_time')(state).substring(0, 10),
     },
   ],
